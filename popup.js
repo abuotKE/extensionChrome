89ec7e6b0fb586809fd87532c9d71ef98bbd1fe1
Adrien <adrien.buot@k-expert.com>
2023-06-03 17:40:34 +0200

    début d'intégration d'une fonction de pointeur

--- popup.js
+++ popup.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let startButton = document.getElementById('startButton');
-    let stopButton = document.getElementById('stopButton');
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+    const pointerButton = document.getElementById('pointerButton');
     startButton.addEventListener('click', startRecording);
     stopButton.addEventListener('click', stopRecording);
+    pointerButton.addEventListener('click', pointElement);
 });
 
 function startRecording() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.runtime.sendMessage("startRecording");
-    });
+    startButton.style.display = 'none';
+    stopButton.style.display = 'block';
+    pointerButton.style.display = 'block';
+    chrome.runtime.sendMessage("startRecording");
 }
 
 function stopRecording() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.runtime.sendMessage("stopRecording");
-    });
+    stopButton.style.display = 'none';
+    startButton.style.display = 'block';
+    pointerButton.style.display = 'none';
+    chrome.runtime.sendMessage("stopRecording");
+}
+
+function pointElement() {
+    chrome.runtime.sendMessage("pointElement");
 }
